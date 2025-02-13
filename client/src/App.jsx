d28b5055159042f4d943b4fc0de605fc2599f18e
Revanth Kanna M <revanthkannam05@gmail.com>
2025-02-13 18:09:01 +0530

    2nd commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { Header } from './components/header'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
