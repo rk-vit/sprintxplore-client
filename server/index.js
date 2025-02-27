54505a9cbb3bd8e7074cb1ffd5278699a5705818
Revanth Kanna M <revanthkannam05@gmail.com>
2025-02-27 21:38:06 +0530

    Backend nodemailer

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
   });
   
 const corsOptions = {
-    origin: "https://sprintxplore.com", 
+    origin: ["https://sprintxplore.com", "https://www.sprintxplore.com"], 
     credentials: true,
     methods: "GET,POST,PUT,DELETE"
 };
